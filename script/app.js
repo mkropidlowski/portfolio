0fcd88817120f0105e72be87d1102d3f75d1ed87
mkropidlowski <mkropidlowsky@gmail.com>
2021-01-03 21:18:54 +0100

    moblie version

--- script/app.js
+++ script/app.js
@@ -15,3 +15,54 @@ card.forEach(selectedCard => {
 });
 
 
+
+
+const hamburger = document.querySelector('.hamburger');
+      menuList = document.querySelector('#menuView');
+      bar1 = document.querySelector('.barF');
+      bar2 = document.querySelector('.barS');
+      bar3 = document.querySelector('.barT');
+      menuView = document.getElementById('menuView');
+      
+
+    hamburger.addEventListener('click', e => {
+
+    if(menuList.style.display === "block")
+    {
+        menuList.style.display = "none";
+        bar1.classList.remove('rotate');
+        bar1.classList.remove('barT');
+        bar3.style.display = 'block';
+        bar2.classList.remove('rotate-second');   
+    } else {
+        menuList.style.display = "block"; 
+              bar1.className = 'barF rotate';
+              bar1.classList.add('barT');
+              bar3.style.display = "none";
+              bar2.className = 'barS rotate-second';
+              menuView.className = 'hide';      
+    }
+   
+});
+// window.onscroll = function () { hideMenuWhenScroll()};
+
+function hideMenuWhenScroll()
+{
+    if(window.scrollY > 1)
+    {
+        const hideValue = document.querySelector('.hide');
+        hideValue.style.display = "none";
+        bar1.classList.remove('rotate');
+        bar1.classList.remove('barT');
+        bar3.style.display = 'block';
+        bar2.classList.remove('rotate-second');
+       
+     
+    } 
+}
+
+
+console.log(window.scrollY);
+
+hideMenuWhenScroll();
+
