7b1c4796ac5a303dc14aca3785fc2a412f361694
mkropidlowski <mkropidlowsky@gmail.com>
2021-02-11 20:49:19 +0100

    edit menu app.js

--- script/app.js
+++ script/app.js
@@ -1,21 +1,3 @@
-// const card = document.querySelectorAll('.flip-card-inner');
-// const flipCard = document.querySelectorAll('.flip-card-back');
-
-
-// card.forEach(selectedCard => {
-//     selectedCard.addEventListener('mouseleave', e => {
-
-//         const colorBox = ['#10d965','#dada10','#da4310','#10a8da','#10dada','#1075da','#81c3e4','#0088cc','#ff6666','#f47171','#3333cc','#ff8000','#993333','#cccc00','#39ac39',];
-//         const randomColor = Math.floor(Math.random()*colorBox.length);
-    
-//         flipCard.forEach(flippedCard => {
-//             flippedCard.style.background = colorBox[randomColor];
-//         })
-//     })
-// });
-
-
-
 
 const hamburger = document.querySelector('.hamburger');
       menuList = document.querySelector('#menuView');
@@ -44,25 +26,20 @@ const hamburger = document.querySelector('.hamburger');
     }
    
 });
-// window.onscroll = function () { hideMenuWhenScroll()};
+
+
+window.onscroll = function () { hideMenuWhenScroll()};
 
 function hideMenuWhenScroll()
 {
-    if(window.scrollY > 1)
+    if(document.body.scrollTop > 1 || document.documentElement.scrollTop > 1)
     {
         const hideValue = document.querySelector('.hide');
         hideValue.style.display = "none";
         bar1.classList.remove('rotate');
-        bar1.classList.remove('barT');
         bar3.style.display = 'block';
         bar2.classList.remove('rotate-second');
        
      
     } 
 }
-
-
-console.log(window.scrollY);
-
-hideMenuWhenScroll();
-
