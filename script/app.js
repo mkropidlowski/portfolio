456868e2754474f34d15af9ae49191811151632b
mkropidlowski <mkropidlowsky@gmail.com>
2021-02-11 20:26:07 +0100

    edit menu css

--- script/app.js
+++ script/app.js
@@ -1,18 +1,18 @@
-const card = document.querySelectorAll('.flip-card-inner');
-const flipCard = document.querySelectorAll('.flip-card-back');
+// const card = document.querySelectorAll('.flip-card-inner');
+// const flipCard = document.querySelectorAll('.flip-card-back');
 
 
-card.forEach(selectedCard => {
-    selectedCard.addEventListener('mouseleave', e => {
+// card.forEach(selectedCard => {
+//     selectedCard.addEventListener('mouseleave', e => {
 
-        const colorBox = ['#10d965','#dada10','#da4310','#10a8da','#10dada','#1075da','#81c3e4','#0088cc','#ff6666','#f47171','#3333cc','#ff8000','#993333','#cccc00','#39ac39',];
-        const randomColor = Math.floor(Math.random()*colorBox.length);
+//         const colorBox = ['#10d965','#dada10','#da4310','#10a8da','#10dada','#1075da','#81c3e4','#0088cc','#ff6666','#f47171','#3333cc','#ff8000','#993333','#cccc00','#39ac39',];
+//         const randomColor = Math.floor(Math.random()*colorBox.length);
     
-        flipCard.forEach(flippedCard => {
-            flippedCard.style.background = colorBox[randomColor];
-        })
-    })
-});
+//         flipCard.forEach(flippedCard => {
+//             flippedCard.style.background = colorBox[randomColor];
+//         })
+//     })
+// });
 
 
 
